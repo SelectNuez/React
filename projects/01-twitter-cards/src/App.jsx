01bc639c0760fdfa616bcb5a691aa623b9095613
Jorge Martin <selectnuez@gmail.com>
2023-05-22 18:11:42 +0200

    01-twitter-cards Start

---
+++ projects/01-twitter-cards/src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+  return (
+    <article>
+      <header>
+        <img src="https://unavatar.io/selectNuez" alt="Avatar de SelectNuez" />
+        <div>
+            <strong>SelectNuez</strong>
+            <span>@selectnuez</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>
+            Seguir
+        </button>
+      </aside>
+    </article>
+  );
+}
